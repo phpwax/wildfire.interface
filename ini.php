5c8a77632d5e63ab5eb0c7148c71cc0b8677d4a4
Charles Marshall <charles@oneblackbear.com>
2013-02-06 18:19:12 +0000

    missing view path

--- ini.php
+++ ini.php
@@ -5,5 +5,7 @@ AutoLoader::register_assets("javascripts/wildfire-plugins",__DIR__."/resources/p
 AutoLoader::register_assets("images/wildfire",__DIR__."/resources/public/images/wildfire/");
 AutoLoader::register_assets("tinymce",__DIR__."/resources/public/tinymce/");
 
+AutoLoader::register_view_path("plugin", __DIR__."/view/");
+
 AutoLoader::add_plugin_setup_script(__DIR__."/setup.php");
 ?>
