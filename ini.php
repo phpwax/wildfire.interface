73eed779cd4cf8e97e72e36d7d1060ece5cda519
mikenosko <michal@oneblackbear.com>
2013-02-08 13:38:35 +0000

    including font folder into assets

--- ini.php
+++ ini.php
@@ -3,6 +3,7 @@ AutoLoader::register_assets("stylesheets/wildfire",__DIR__."/resources/public/st
 AutoLoader::register_assets("javascripts/wildfire",__DIR__."/resources/public/javascripts/wildfire/", "/*.js");
 AutoLoader::register_assets("javascripts/wildfire-plugins",__DIR__."/resources/public/javascripts/wildfire-plugins/", "/*.js");
 AutoLoader::register_assets("images/wildfire",__DIR__."/resources/public/images/wildfire/");
+AutoLoader::register_assets("fonts/wildfire",__DIR__."/resources/public/fonts/wildfire/","/*");
 AutoLoader::register_assets("tinymce",__DIR__."/resources/public/tinymce/");
 
 AutoLoader::register_view_path("plugin", __DIR__."/view/");
