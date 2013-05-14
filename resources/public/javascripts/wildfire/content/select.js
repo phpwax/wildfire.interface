94a1bb4918e57c49987ac83276c733a767fc1baa
mikenosko <michal@oneblackbear.com>
2013-05-14 13:59:58 +0100

    select2 for select boxes

---
+++ resources/public/javascripts/wildfire/content/select.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function(){
+
+  jQuery("select:not(.unstyled_select)").each(function(){
+
+    var select2 = jQuery(this).select2({width:"resolve",allowClear: true}),
+        option = jQuery(this).find("option:first");
+        if(!option.val()){
+          placeholder = option.html();
+          option.html("");
+          select2.select2({placeholder:placeholder}); //would be nice if there would be a setter for the placeholder
+        }
+
+  });
+
+});
