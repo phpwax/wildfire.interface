b72032478bea2edac4b8090c85050c6c1a394fe4
Sheldon Els <sheldon.els@gmail.com>
2013-05-14 16:58:03 +0100

    resolve causes fixed widths in inline styles

--- resources/public/javascripts/wildfire/content/select.js
+++ resources/public/javascripts/wildfire/content/select.js
@@ -2,12 +2,12 @@ jQuery(document).ready(function(){
 
   jQuery("select:not(.unstyled_select)").each(function(){
 
-    var select2 = jQuery(this).select2({width:"resolve",allowClear: true}),
+    var select2 = jQuery(this).select2({width:"off",allowClear: true}),
         option = jQuery(this).find("option:first");
         if(!option.val()){
           placeholder = option.html();
           option.html("");
-          select2.select2({width:"resolve",allowClear: true,placeholder:placeholder}); //would be nice if there would be a setter for the placeholder
+          select2.select2({width:"off",allowClear: true,placeholder:placeholder}); //would be nice if there would be a setter for the placeholder
         }
 
   });
