8296baf9f384bef65cdb69d5ce41eeb6dcea49b6
mikenosko <michal@oneblackbear.com>
2013-05-20 16:27:14 +0100

    fixes to url_history

--- resources/public/javascripts/wildfire/url_history.js
+++ resources/public/javascripts/wildfire/url_history.js
@@ -1,22 +1,26 @@
 jQuery(function($){
-  var parse = function(url){
+  var parse_url = function(url){
     var orig = url.split("?"),
         after = orig[1] || orig[0],
         params = after.split("&"),
         ret = {};
     for(var i in params){
-      var split = params[i].split("=");
-      ret[split[0]] = split[1];
+
+      if(params[i] != window.location.origin+window.location.pathname){
+        var split = params[i].split("=");
+        ret[split[0]] = split[1];
+      }
     }
     return ret;
   }
 
   $(window).bind("update_url", function(e, new_params){
-    if(typeof new_params == "string") new_params = parse(new_params);
-    var current_params = parse(window.location.href);
+    if(typeof new_params == "string") new_params = parse_url(new_params);
+    var current_params = parse_url(window.location.href);
     $.extend(current_params, new_params);
     var ret = [];
     for(var i in current_params) ret.push(i+"="+current_params[i]);
-    history.pushState({}, "", window.location.pathname+"?"+ret.join("&"));
+    console.log(current_params);
+    history.pushState({}, window.document.title, window.location.pathname+"?"+ret.join("&"));
   });
 });
