2a38a461ff3e9d6cd9ad1a707c4a9f6a06b0a935
mikenosko <michal@oneblackbear.com>
2013-06-28 12:25:55 +0100

    tweaks for inline image tinymce js

--- resources/public/javascripts/wildfire/tinymce/_init.js
+++ resources/public/javascripts/wildfire/tinymce/_init.js
@@ -58,30 +58,19 @@ jQuery(document).ready(function(){
   //source code view
   jQuery('#wildfire-source-code').hide();
   //inline image picker
-  jQuery(window).bind("filter.trigger", function(){    
-    var cloned_files = jQuery("#media .file-listing").clone(),
-        info_box = jQuery(document.createElement("div"))
+
+
+  jQuery("#wildfire-image-dialog").on("click",".media-listing-item", function(e){
+
+    e.preventDefault();
+
+    var row = jQuery(this),
+        preview_container = row.closest("#wildfire-image-dialog").find(".image_preview"),
+        trigger_type = (jQuery(this).data("media") ? jQuery(this).data("media") : "generic")
         ;
-    //replace with the info
-    info_box.addClass("image-info-container").hide().append("<div class='image-info'><label for='wf_img_size'>Size</label><input type='text' name='wf_img_size' value='120' id='wf_img_size'><label for='wf_img_pos'>Align</label><select name='wf_img_pos' id='wf_img_pos'><option value='inline_image align_left'>Left</option><option value='inline_image align_right'>Right</option><option value='inline_image align_center'>Center</option></select><label for='wf_img_cap'>Description</label><input type='text' name='wf_img_cap' value='' id='wf_img_cap'></div>");
-    cloned_files.find(".uploading_container").replaceWith(info_box);
-    
-    jQuery("#wildfire-image-dialog").addClass("media-listing").html(jQuery(cloned_files).html()).hide().ajaxSuccess(function(){}).find("input[type='text'],select").each(function(){
-      var obj = jQuery(this), parent_form = obj.parents("#wildfire-image-dialog");
-      jQuery(window).trigger("filter.bind", [obj, parent_form]);
-    });
-    jQuery("#wildfire-image-dialog tbody tr").live("click", function(e){
-      e.preventDefault();
 
-      var row = jQuery(this),
-      preview_container = (row.closest(".media-listing").find(".media-data").length) ? row.closest(".media-listing").find(".media-data") : jQuery("#wildfire-image-dialog .upload_block .media-data"),
-      trigger_type = (jQuery(this).data("media") ? jQuery(this).data("media") : "generic")
-      ;
-
-      jQuery(window).trigger("media."+trigger_type+".preview", [row, preview_container]);
-      jQuery("#wildfire-image-dialog .image-info-container").show();
-    });
-    jQuery(window).unbind("filter.trigger");
+    jQuery(window).trigger("media."+trigger_type+".preview", [row, preview_container]);
+    //jQuery("#wildfire-image-dialog .image-info-container").show();
   });
 
 
