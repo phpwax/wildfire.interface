279e7a9f51eb3994bb8392c4fcdb8773d951443f
mikenosko <michal@oneblackbear.com>
2013-07-04 15:53:50 +0100

    not needed

--- resources/public/javascripts/wildfire/hover-info.js
+++ resources/public/javascripts/wildfire/hover-info.js
@@ -5,34 +5,6 @@ jQuery(function(){
   });
 
 
-  jQuery(".hover_update").each(function(){
-    var update = jQuery(this),
-        els = update.find(".hover_el");
-
-    var delay;
-    jQuery(update.attr("data-hover-selector")).live('mouseover', function(){
-      if(!jQuery(this).data('init')){
-        jQuery(this).data('init', true);
-        jQuery(this).hoverIntent({over:function(){
-          var hover_target = jQuery(this);
-          jQuery(update.attr("data-hover-selector")).removeClass("active");
-          hover_target.addClass("active");
-
-          els.each(function(){
-            var el = jQuery(this);
-            var data_hover_fetch = el.attr("data-hover-fetch");
-            if(data_hover_fetch) el.text(hover_target.attr(data_hover_fetch));
-            var data_hover_fetch_href = el.attr("data-hover-fetch-href");
-            if(data_hover_fetch_href) el.attr("href", hover_target.attr(data_hover_fetch_href));
-          });
-        }, timeout:400});
-        jQuery(this).trigger('mouseover');
-      }
-    });
-
-  });
-
-
   jQuery(".preview-hover tbody tr, .cms-uploads-1 tbody tr").live("mouseover", function(e){
     var str = "",
         row = jQuery(this),
