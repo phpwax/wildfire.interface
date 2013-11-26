dc47453e839fef4a3f933e85fa0939d23dde37ac
Charles Marshall <charles@oneblackbear.com>
2013-11-26 11:58:46 +0000

    working on converting link dialog to tabs and multi functions

--- resources/public/tinymce/jscripts/tiny_mce/plugins/wflink/editor_plugin.js
+++ resources/public/tinymce/jscripts/tiny_mce/plugins/wflink/editor_plugin.js
@@ -8,6 +8,44 @@
  * Contributing: http://tinymce.moxiecode.com/contributing
  */
 
+function __tinymce_insert_standard_link(ed, se){
+  var e = ed.dom.getParent(se.getNode(), 'A'),
+      linkd=jQuery('#wildfire-link-dialog'),
+      siteaddress = linkd.attr("data-server");
+      href=(jQuery('#wf_ld_internal') && jQuery('#wf_ld_internal').val())?jQuery('#wf_ld_internal').val(): jQuery('#wf_ld_url').val(),
+      href_target= jQuery('#wf_ld_tar').val(),
+      href_class = jQuery('#wf_ld_class').val();
+
+  if(href.length && href_target.length){
+    if(href.charAt(0) == "/") href = siteaddress+href;
+    else if(href.substring(0,7) == "mailto:") href=href;
+    else if(href.substring(0,4) != "http") href = "http://"+href;
+    // Create new anchor elements
+    if (e == null) {
+    ed.getDoc().execCommand("unlink", false, null);
+      tinymce.execCommand("mceInsertLink", false, "#mce_temp_url#", {skip_undo : 1});
+      tinymce.each(ed.dom.select("a"), function(n) {
+        if (ed.dom.getAttrib(n, 'href') == '#mce_temp_url#') {
+          e = n;
+          ed.dom.setAttribs(e, {
+           href : href,
+           target : href_target,
+           class: href_class
+          });
+        }
+      });
+    } else {
+      ed.dom.setAttribs(e, {
+        href : href,
+        target : href_target,
+                    class: href_class
+      });
+    }
+  }
+}
+
+var __tinymce_insert_functions = ["__tinymce_insert_standard_link"];
+
 (function() {
   tinymce.create('tinymce.plugins.WildfireLinkPlugin', {
     init : function(ed, url) {
@@ -23,41 +61,15 @@
         ed.windowManager.open({
           ui_dialog: "#wildfire-link-dialog",
           width : 400,
-          height : 300,
+          height : 500,
           inline : 1,
           button_actions:{
             'Insert':function(){
-              var e = ed.dom.getParent(se.getNode(), 'A'),
-                  linkd=jQuery('#wildfire-link-dialog'),
-                  siteaddress = linkd.attr("data-server");
-                  href=(jQuery('#wf_ld_internal') && jQuery('#wf_ld_internal').val())?jQuery('#wf_ld_internal').val(): jQuery('#wf_ld_url').val(),
-                  href_target=(jQuery('#wf_ld_tar').val()),
-                  href_class = (jQuery('#wf_ld_class').val())
-                  ;
-              if(href.charAt(0) == "/") href = siteaddress+href;
-              else if(href.substring(0,7) == "mailto:") href=href;
-              else if(href.substring(0,4) != "http") href = "http://"+href;
-              // Create new anchor elements
-              if (e == null) {
-              ed.getDoc().execCommand("unlink", false, null);
-                tinymce.execCommand("mceInsertLink", false, "#mce_temp_url#", {skip_undo : 1});
-                tinymce.each(ed.dom.select("a"), function(n) {
-                  if (ed.dom.getAttrib(n, 'href') == '#mce_temp_url#') {
-                    e = n;
-
-                    ed.dom.setAttribs(e, {
-                     href : href,
-                     target : href_target,
-                     class: href_class
-                    });
-                  }
-                });
-              } else {
-                ed.dom.setAttribs(e, {
-                  href : href,
-                  target : href_target,
-                              class: href_class
-                });
+              for(var i in __tinymce_insert_functions){
+                var func = __tinymce_insert_functions[i];
+                console.log(func);
+                window[func](ed,se);
+                //(ed,se);
               }
               jQuery(this).dialog("close");
             },
